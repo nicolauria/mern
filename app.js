6dd5b30f3a4b8cae7c204813528a7ba206e058cb
nico <nicolauria@outlook.com>
2019-05-02 15:14:35 -0700

    basic user auth

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server is running on port ${port}.`))
 
-app.get('/', (req, res) => res.send('Hello world'));
-
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
 mongoose.connect(db, { useNewUrlParser: true })
   .then(() => console.log('connected to mongoDB'))
   .catch(err => console.log(err));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const users = require('./routes/api/users');
+
+app.use('/api/users', users);
+
+const passport = require('passport');
+app.use(passport.initialize());
+require('./config/passport')(passport);
